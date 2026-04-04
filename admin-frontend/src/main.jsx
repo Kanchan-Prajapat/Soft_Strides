3d9e0dbc468beeb7dc6d96a99282852c776fb365
kp <kanchanprajapat676@gmail.com>
2026-04-04 19:29:59 +0530

    wapas se auth change in admin

--- admin-frontend/src/main.jsx
+++ admin-frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <ToastProvider>
-   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+   <GoogleOAuthProvider clientId={import.meta.env.VITE_ADMIN_GOOGLE_CLIENT_ID}>
     <App />
     </GoogleOAuthProvider>
   </ToastProvider>
