843dc8f4f3a14133487841cbdb98a9adc65ded7a
kp <kanchanprajapat676@gmail.com>
2026-05-25 19:22:47 +0530

    protectedroute

--- admin-frontend/src/components/ProtectedRoute.jsx
+++ admin-frontend/src/components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem("adminToken");
+  const token = localStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/admin/login" replace />;
+   return <Navigate to="/" replace />;
   }
 
   return children;
